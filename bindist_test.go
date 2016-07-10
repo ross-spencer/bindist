ef1d5cd18c6fb1b21903f7bf0d174a88d007eb58
ross-spencer <allalongthewatchtower2001@gmail.com>
2016-07-11 11:35:49 +1200

    [testing] Real tests.

--- bindist_test.go
+++ bindist_test.go
@@ -10,7 +10,7 @@ func TestExportContains(t *testing.T) {
    var n1 = []byte{0xca, 0xfe, 0xba, 0xbe}
 
    //small needle 
-   /*var h2 = []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xab, 0x00, 0x00, 0x00, 0x00}
+   var h2 = []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xab, 0x00, 0x00, 0x00, 0x00}
    var n2 = []byte{0xab}
 
    //needle isn't in the haystack
@@ -23,11 +23,30 @@ func TestExportContains(t *testing.T) {
 
    //needle at end of file
    var h5 = []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff}
-   var n5 = []byte{0xff}*/
+   var n5 = []byte{0xff}
 
    found1, offset1 := ExportContains(n1,h1)
    if found1 != true && offset1 != 3 {
    	t.Error("Needle not found in haystack when it should have been.")
    }
 
+   found2, offset2 := ExportContains(n2,h2)
+   if found2 != true && offset2 != 7 {
+   	t.Error("Needle not found in haystack when it should have been.")
+   }
+
+   found3, offset3 := ExportContains(n3,h3)
+   if found3 != false && offset3 != 0 {
+   	t.Error("Needle not found in haystack when it should have been.")
+   }
+
+   found4, offset4 := ExportContains(n4,h4)
+   if found4 != true && offset4 != 0 {
+   	t.Error("Needle not found in haystack when it should have been.")
+   }
+
+   found5, offset5 := ExportContains(n5,h5)
+   if found5 != true && offset5 != 11 {
+   	t.Error("Needle not found in haystack when it should have been.")
+   }
 }
