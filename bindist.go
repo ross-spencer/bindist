206c2c9eaeaf884b6636bf80a633dda4146c827c
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2016-07-15 23:50:49 +1200

    [refactor] Remove newly unused code.

--- bindist.go
+++ bindist.go
@@ -39,42 +39,6 @@ func init() {
    flag.BoolVar(&vers, "version", false, "[Optional] Return version of bindist.")
 }
 
-func getbfsize(fsize int64, pos int64) int64 {
-   newsize := (pos - fsize)
-   if newsize > 0 && newsize < bfsize {
-      bfsize = newsize
-   }
-   return bfsize
-}
-
-func contains(needle []byte, haystack []byte) (bool, int) {
-
-	nlen := len(needle)
-	xlen := len(haystack)
-
-	var offset int
-
-	for x := 0; x < xlen; x += 1 {
-		if bytes.Equal(needle, haystack[:nlen]) { //check two slices are equal
-			return true, offset
-		} else {
-			//iterate through haystack comparing two by two...
-			haystack = deletefromslice(1, haystack)
-		}
-		offset += 1
-	}
-
-	return false, offset
-}
-
-func deletefromslice(n int, slice []byte) []byte {      //return false if no buffer left?
-   //Slice Tricks: a = append(a[:i], a[i+1:]...) 
-   for x:=0; x<n; x+=1 {
-      slice = append(slice[:0], slice[0+1:]...)
-   }
-   return slice
-}
-
 func outputResult(found bool, offset1, offset2 int, fi os.FileInfo) {
    
    //Have reached end of file without finding both sequences :(
@@ -154,62 +118,15 @@ func handleFile(fp *os.File, fi os.FileInfo) {
             return
          }
       }
-
       //must call last to enable last iteration over buffer...
       if err == io.EOF {
          //we haven't returned so far and so we haven't found our values
          outputResult(found, 0, 0, fi)
          return
       }
-
       start, buf = moveWindow(buf, 0, 0)
    }  
 }
-
-/*func _handleFile(fp *os.File, fi os.FileInfo) {
-
-   var found1, found2 bool
-   var tmpoff, offset1, offset2 int
-   
-   var eof int64 = fi.Size()
-   var pos int64 = 0
-
-   // read file, control how we reach EOF
-   for pos < eof {
-      //fmt.Fprintln(os.Stderr, "Buffer required: ", getbfsize(pos, fi.Size()))
-
-      buf := make([]byte, bfsize)
-
-      _, err := fp.Read(buf)
-      if err != nil {
-         fmt.Fprintln(os.Stderr, "ERROR: Error reading bytes: ", err)
-         break
-      }
-
-      if found1 == false {
-         found, offset := contains(byteval1, buf)
-         tmpoff += offset
-         if found == true {
-            //we don't need to look for byteval1 any more
-            found1 = true
-            offset1 = tmpoff
-         }
-      }
-
-      if found1 == true {
-         found, offset := contains(byteval2, buf)
-         tmpoff += offset
-         if found == true && found2 == false {
-            found2 = true
-            offset2 = tmpoff
-            break
-         }
-      }
-
-      //equivalent to ftell() in C
-      pos, _ = fp.Seek(0, os.SEEK_CUR) 
-   }
-}*/
 
 //callback for walk needs to match the following:
 //type WalkFunc func(path string, info os.FileInfo, err error) error
@@ -283,9 +200,7 @@ func validateArgsAndGo() {
 
 func main() {
    flag.Parse()
-
    var verstring = "bindist version"
-
    if vers {
       fmt.Fprintf(os.Stderr, "%s %s \n", verstring, version)
       os.Exit(0)
@@ -298,6 +213,5 @@ func main() {
       flag.Usage()
       os.Exit(0)
    }
-
    validateArgsAndGo()
 }
